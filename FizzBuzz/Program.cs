be571a19fdaef9ae5284c95560dc187e89d8935b
Tim Leach <timleach635@gmail.com>
2022-02-21 10:51:41 +0000

    Get FizzBuzz working for rules 3 and 5

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,27 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (output == "")
+                {
+                    output = i.ToString();
+                }
+
+                Console.WriteLine(output);
+            }
         }
     }
 }
